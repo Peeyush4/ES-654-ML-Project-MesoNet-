45681a5811f59516651bdd02501bbba197762465
Psychiquest <rushil0908@gmail.com>
2020-03-31 12:29:12 +0530

    a

--- combine_metadata.py
+++ combine_metadata.py
@@ -2,6 +2,6 @@ import os,json
 all_metadata=dict()
 for folder in os.listdir(".."):
     if folder.startswith("dfdc"):
-        metadata=json.loads(open(folder+"/metadata.json","r"))
+        metadata=json.load(open("../"+folder+"/metadata.json","r"))
         all_metadata.update(metadata)
 json.dump(all_metadata,open("all_metadata.json","w"))
