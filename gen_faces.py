0cd269d33a527e2c88ab72c72cfe3dae3987bcf5
Psychiquest <rushil0908@gmail.com>
2020-04-03 18:55:20 +0530

    added a functionality to check if there is a previous work done to not repeat it again

--- gen_faces.py
+++ gen_faces.py
@@ -10,10 +10,13 @@ from math import floor
 import cv2 as cv
 from scipy.ndimage.interpolation import zoom
 
-FACE_DICT_FILE_NAME="face_dict.json"
-FINAL_IMAGES_FOLDER="face_images_2/"
+FACE_DICT_FILE_NAME="train_face_dict.json"
+FINAL_IMAGES_FOLDER="train_face_images/"
+
+work_done_previously=True
 
 if not isdir(FINAL_IMAGES_FOLDER):
+    work_done_previously=False
     mkdir(FINAL_IMAGES_FOLDER)
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
@@ -43,7 +46,7 @@ class Video:
         self.path = path
         self.container = imageio.get_reader(path, 'ffmpeg')
         self.length = self.container.count_frames()
-        self.fps = self.container.get_meta_data()['fps']
+        # self.fps = self.container.get_meta_data()['fps']
         self.images={}
         self.faces={}
     
@@ -62,23 +65,46 @@ class Video:
     def __len__(self):
         return self.length
 
+def filter_facedict(facedict,worked_filenames):
+    original_dictionary=json.load(open(facedict,"r"))
+    for key in original_dictionary:
+        if key.split("_")+".mp4" in worked_filenames:
+            pass
+        else:
+            print("not there")
+            # del original_dictionary[key]
+    json.dump(original_dictionary,open(facedict,"w"))
+
 def generate_faces_and_facedict(dirname,meta_data_file,frame_subsample_count=30,batch_size=2):
     
     # remove(FACE_DICT_FILE_NAME) # To remove previous versions
 
-    filenames = [f for f in listdir(dirname) if isfile(join(dirname, f)) and ((f[-4:] == '.mp4') or (f[-4:] == '.avi') or (f[-4:] == '.mov'))]
+    all_filenames = [f for f in listdir(dirname) if isfile(join(dirname, f)) and (f[-4:] == '.mp4')]
+    
+    if not work_done_previously:
+        filenames=all_filenames
+    elif not isfile(FACE_DICT_FILE_NAME):
+        filenames=all_filenames
+    else:
+        files_worked_previously=listdir(FINAL_IMAGES_FOLDER)
+        videos_worked_previously=list(set([filename.split("_")[0]+".mp4" for filename in files_worked_previously]))
+        print("Previously worked videos: ",len(videos_worked_previously))
+        filenames=all_filenames-videos_worked_previously
+        filter_facedict(FACE_DICT_FILE_NAME,filenames)
+
+
     meta_data=json.load(open(meta_data_file,"r"))
     thread_list=[]
     for vid in tqdm(filenames):
 
-        print('Dealing with video ', vid)
+        # print('Dealing with video ', vid)
         face_label=meta_data[vid]['label']
         video=Video(join(dirname, vid))
         skipstep = max(floor(video.length / frame_subsample_count), 0)
         finder_frameset = range(0, video.length, skipstep + 1)
 
         face_counter=0
-        a=time()
+        # a=time()
         for lb in np.arange(0, len(finder_frameset), batch_size):
             imgs = [video.get(i) for i in finder_frameset[lb:lb+batch_size]]
             multiple_frame_faces=mtcnn(imgs)
@@ -98,14 +124,15 @@ def generate_faces_and_facedict(dirname,meta_data_file,frame_subsample_count=30,
                     video.faces[vid[:-4]+"_"+str(face_counter)]=face_label
                     video.images[vid[:-4]+"_"+str(face_counter)]=frame_faces
                     face_counter+=1
-        b=time()
+        # b=time()
         store_json(FACE_DICT_FILE_NAME,video.faces)
         p1=threading.Thread(target=store_images,args=(video.images,))
         p1.start()
+        del video
         thread_list.append(p1)
         # store_images(video.images)
-        c=time()
-        print("Time taken for finding all faces: ",b-a," Time taken for storing all faces: ",c-b)
+        # c=time()
+        # print("Time taken for finding all faces: ",b-a," Time taken for storing all faces: ",c-b)
     for thread in thread_list:
         thread.join()
-generate_faces_and_facedict("train_videos","all_metadata.json",batch_size=5)
+generate_faces_and_facedict("train_videos","all_metadata.json",batch_size=10)
--- train_test_split.py
+++ train_test_split.py
@@ -1,5 +1,7 @@
 import os, shutil, numpy as np
 files = os.listdir("train_videos")
-files = np.random.choice(files, 600, replace = False)
+files = np.random.choice(files, int(0.3*len(files)), replace = False)
+if not os.path.isdir("test_videos"):
+    os.mkdir("test_videos")
 for file in files:
         shutil.move("train_videos/"+file, "test_videos")
